5c7d910c800a2db346a00380750e585a23f5b73d
Alatr <dimalukianovweb@gmail.com>
2020-12-23 13:05:15 +0200

    refactor -rm all global vars

--- src/index.js
+++ src/index.js
@@ -1,28 +1,5 @@
 import readlineSync from 'readline-sync';
 
-let correctAnswerCnt = 0;
-
-function defineNextStep(generateTaskFn, userName) {
-  if (correctAnswerCnt === 3) {
-    console.log(`Congratulations, ${userName}!`);
-    return;
-  }
-
-  const task = generateTaskFn();
-
-  console.log(task.question);
-  const answer = readlineSync.question('Your answer:');
-
-  if (answer === task.correctAnswer) {
-    correctAnswerCnt += 1;
-    console.log('Correct!');
-    defineNextStep(generateTaskFn, userName);
-    return;
-  }
-  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${task.correctAnswer}'.`);
-  console.log(`Let's try again, ${userName}!`);
-}
-
 export default function gameCore({ generateTask, explanationMsg }) {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name?');
@@ -30,5 +7,25 @@ export default function gameCore({ generateTask, explanationMsg }) {
   console.log(`Hello, ${userName}!`);
   console.log(explanationMsg);
 
-  defineNextStep(generateTask, userName);
+  function defineNextStep(correctAnswerCnt = 0) {
+    if (correctAnswerCnt === 3) {
+      console.log(`Congratulations, ${userName}!`);
+      return;
+    }
+
+    const task = generateTask();
+
+    console.log(task.question);
+    const answer = readlineSync.question('Your answer:');
+
+    if (answer === task.correctAnswer) {
+      console.log('Correct!');
+      defineNextStep(correctAnswerCnt + 1);
+      return;
+    }
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${task.correctAnswer}'.`);
+    console.log(`Let's try again, ${userName}!`);
+  }
+
+  defineNextStep();
 }
