0666422457a845d0667ae26527b6454ac296c40e
Alatr <dimalukianovweb@gmail.com>
2020-12-23 16:33:32 +0200

    check hex action

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export default function gameCore({ generateTask, gameRule }) {
+export default function runGame({ generateTask, gameRule }) {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name?');
 
