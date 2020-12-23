47415267109e308a9388f41919f355dd88086edc
Alatr <dimalukianovweb@gmail.com>
2020-12-23 18:50:03 +0200

    fix is prime loop

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,10 +2,10 @@ import runGame from '../index.js';
 import getRandomNum from '../get-random-num.js';
 
 const isPrime = (num) => {
-  for (let i = 2; i < num; i + 1) {
+  for (let i = 2; i < num; i += 1) {
     if (num % i === 0) return false;
   }
-  return num > 1;
+  return true;
 };
 
 const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
