64d521d4b2bcc17bca186ed25d227d459b76d0ad
Alatr <dimalukianovweb@gmail.com>
2020-12-23 19:01:39 +0200

    q

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,6 +2,9 @@ import runGame from '../index.js';
 import getRandomNum from '../get-random-num.js';
 
 const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
   for (let i = 2; i < num; i += 1) {
     if (num % i === 0) return false;
   }
