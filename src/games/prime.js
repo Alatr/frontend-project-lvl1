96cd1bff08d32e67fd8b964196eec07b33ee6757
Alatr <dimalukianovweb@gmail.com>
2020-12-29 10:12:01 +0200

    fix prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,8 +5,8 @@ const isPrime = (num) => {
   if (num < 2) {
     return false;
   }
-  for (let i = 2; i < num; i += 1) {
-    if (num % 2 === 0) return false;
+  for (let i = 2; i < num / 2; i += 1) {
+    if (num % i === 0) return false;
   }
   return true;
 };
